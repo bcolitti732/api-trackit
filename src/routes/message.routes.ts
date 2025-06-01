9ec7cdadf5ae5b15c7d330e25e7067e470f3cee0
AliciaCarmonaLopez <alicia.carmona@estudiantat.upc.edu>
2025-06-01 19:47:53 +0200

    autenticacion en todos endpoints

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { postMessage, getMessagesBetweenUsers, getMessagesByUser, acknowledgeMessage, getUserContacts, startConversation} from '../controllers/message.controller';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.post('/', postMessage);
-router.get('/contacts/:userId', getUserContacts);
-router.get('/:user1Id/:user2Id', getMessagesBetweenUsers);
-router.get('/:userId', getMessagesByUser);
-router.put('/acknowledge', acknowledgeMessage);
-router.post('/start', startConversation);
+router.post('/', authMiddleware,postMessage);
+router.get('/contacts/:userId',authMiddleware,getUserContacts);
+router.get('/:user1Id/:user2Id', authMiddleware,getMessagesBetweenUsers);
+router.get('/:userId', authMiddleware,getMessagesByUser);
+router.put('/acknowledge', authMiddleware,acknowledgeMessage);
+router.post('/start', authMiddleware, startConversation);
 
 export default router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -17,21 +17,22 @@ import {
     getOptimizedRoute
 } from '../controllers/user.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
+import { auth } from 'google-auth-library';
 
-router.get("/", getAllUsers);
-router.post("/", postUser);
+router.get("/",authMiddleware, getAllUsers);
+router.post("/",authMiddleware, postUser);
 router.get("/me", authMiddleware, (req, res, next) => {
     req.params.name = req.user?.name;
     next();
 }, getUserByName);
-router.get('/:id', getUserById);
-router.get('/name/:name', getUserByName);
-router.put('/:id', updateUserById);
-router.delete('/:id', deleteUserById);
-router.put('/:id/deactivate', deactivateUserById);
+router.get('/:id',authMiddleware, getUserById);
+router.get('/name/:name', authMiddleware,getUserByName);
+router.put('/:id', authMiddleware,updateUserById);
+router.delete('/:id',authMiddleware, deleteUserById);
+router.put('/:id/deactivate', authMiddleware,deactivateUserById);
 router.get('/:id/packets', authMiddleware, getUserPackets);
-router.get('/:id/assigned-packets', getAssignedPackets);
-router.post('/:name/packets', addPacketToUser);
-router.post('/:id/assign-packet',assignPacket);
-router.get('/:id/optimized-route', getOptimizedRoute);
+router.get('/:id/assigned-packets', authMiddleware,getAssignedPackets);
+router.post('/:name/packets', authMiddleware,addPacketToUser);
+router.post('/:id/assign-packet',authMiddleware,assignPacket);
+router.get('/:id/optimized-route', authMiddleware,getOptimizedRoute);
 export default router;
